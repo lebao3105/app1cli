134b4a7b47cbbf7c3f1de1c38fe19026b07a7965
Le Bao Nguyen <bao12345yocoo@gmail.com>
2021-10-28 14:34:52 +0700

    New version 1.0.26

--- app1.pas
+++ app1.pas
@@ -1,20 +1,19 @@
 Program App1;
-uses crt;
+uses
+    crt;
+
 label
-    menu, compare, sub_menu, exit_program, sub_menu_cal, cal, info;
+    menu, sub_menu, exit_program, sub_menu_cal, cal;
 
 var
    choice : 1 .. 4;
-   sub_choice_cal : 1 .. 6;
+   sub_choice_cal : 1 .. 10;
    sub_choice : 1 .. 3;
    number1, number2, y, answer : integer;
    number3, number4, x : real;
-                               
-  begin
+
+begin
     TextColor(Red);
-    // Use the Ubuntu 6.10 Beta startup sound
-    // This is not working yet without MMSystem unit, which is not available now.
-    // sndPlaysound('../sounds/Ubuntu_6.10_Beta_Startup.wav', snd_Async);
     writeln('Starting...');
     TextColor(White);
     delay(3000);
@@ -22,11 +21,11 @@ var
     writeln('App1 by Le Bao Nguyen.');
     delay(500);
     TextColor(2);
-    writeln('Version 1.0.1');
+    writeln('Version 1.0.26');
     TextColor(White);
     delay(1000);
     clrscr;
-    writeln('Welcome to App1! Choose your choice to use this app now:');
+    writeln('Welcome to app1! Choose one function to use this app now:');
 //Main Menu
   menu:
    begin
@@ -42,7 +41,6 @@ var
   end;
  //compare
  if choice = 1 then
- compare:
   begin
    writeln('Type your number:');
    TextColor(White);
@@ -88,6 +86,7 @@ cal:
    begin
      writeln('Type numbers:');
      write('Number 1: '); Textcolor(2); readln(number1);
+     TextColor(White);
      write('Number 2: '); Textcolor(2); readln(number2);
      TextColor(White);
      x := number1 + number2;
@@ -98,6 +97,7 @@ cal:
    begin
      writeln('Type numbers:');
      write('Number 1: '); Textcolor(2); readln(number1);
+     TextColor(White);
      write('Number 2: '); Textcolor(2); readln(number2);
      TextColor(White);
      x := number1 - number2;
@@ -146,8 +146,12 @@ cal:
     writeln('3.x^2 - a^2');
     writeln('4.(a+b)^2');
     writeln('5.(a-b)^2');
+    writeln('6.a^3 - b^3 (New)');
+    writeln('7.(a+b)^3 (New)');
+    writeln('8.(a-b)^3 (New)');
     TextColor(Red);
-    writeln('6.Cancel');
+    writeln('9.Cancel');
+    writeln('10.Force exit the program');
     TextColor(White);
     write('Enter your choice: '); TextColor(2); readln(sub_choice_cal);
     TextColor(White);
@@ -192,6 +196,7 @@ cal:
      write('Number 2: '); TextColor(White); readln(number2);
      TextColor(White);
      answer := sqr(number1) - sqr(number2);
+     writeln('Now you have: ', number1 ,'^2 - ',number2, '^2' );
      writeln('Answer is: ', answer);
      goto sub_menu_cal;
    end;
@@ -205,6 +210,7 @@ cal:
      write('Number 2: '); TextColor(2); readln(number2);
      TextColor(White);
      answer := sqr(number1 + number2);
+     writeln('You have: (', number1, ' + ',number2,')^2');
      writeln('Answer is: ', answer);
      goto sub_menu_cal;
    end;
@@ -218,35 +224,86 @@ cal:
     write('Number 2: '); TextColor(2); readln(number2);
     TextColor(White);
     answer := Sqr(number1 - number2);
-    writeln('Answer is: ', answer);
+    writeln('Now we have:', '(', number1, '-', number2,')', '^2');
+    writeln('The answer is: ', answer);
     goto sub_menu_cal;
   end;
 
-  //cancel:
+  //a^3 + b^3 (ver 1.0.26)
   if sub_choice_cal = 6 then
+   begin
+    write('Type number 1 (a): '); TextColor(2); readln(number1);
+    TextColor(White);
+    write('Number 2 (b): '); TextColor(2); readln(number2);
+    TextColor(White);
+    answer := number1*number1*number1 + number2*number2*number2;
+    writeln('Here is what you got: ', number1, '^3 + ',number2, '^3');
+    writeln('The answer is: ', answer);
+    goto sub_menu_cal;
+   end;
+
+  //(a+b)^3 (ver 1.0.26)
+  if sub_choice_cal = 7 then
+   begin
+    write('Type number 1 (a): '); TextColor(2); readln(number1);
+    TextColor(white);
+    write('Type number 2 (b): '); TextColor(2); readln(number2);
+    TextColor(white);
+    answer := (number1 + number2) * (number1 + number2) * (number1 + number2);
+    writeln('Here is what you got: (', number1, '+', number2, ')^3');
+    writeln('Answer: ', answer);
+    goto sub_menu_cal;
+   end;
+
+  //(a-b)^3 (ver 1.0.26)
+  if sub_choice_cal = 8 then
+   begin
+    write('Type number 1 (a): '); TextColor(2); readln(number1);
+    TextColor(White);
+    write('Type number 2 (b): '); TextColor(2); readln(number2);
+    TextColor(White);
+    answer := (number1 - number2) * (number1 - number2) * (number1 - number2);
+    writeln('Heres what you have: (',number1, '-',number2,')^3');
+    writeln('The answer is: ', answer);
+    goto sub_menu_cal;
+   end;
+
+  //Cancel:
+  if sub_choice_cal = 8 then
    begin
     clrscr;
     delay(500);
     goto menu;
    end;
-   end; //end for 6.Advanced
+
+  //Exit
+  if sub_choice_cal = 9 then
+   goto exit_program;
+    end; //end for 6.Advanced
    end; //end for choice = 2
 
   //Infomation(s)
   if choice = 3 then
    begin
-   info:
     clrscr;
   //About
     writeln('About this app:');
     writeln('App1 Version 1.0.25');
-    writeln('This application is written by Le Bao Nguyen in Pascal. This is a free software, so you can edit it, or redistribution it to everyone.');
+    writeln('This application is written by Le Bao Nguyen in Pascal.');
+    writeln('You can edit it, or redistribution it to everyone.');
     writeln('This software follows the GNU V3 license.');
     writeln('"Press Enter to continue"');
     Readln();
-  //Features
+  //News
+   //Curent
     writeln('Whats new on this version:');
-    writeln('Current Version: 1.0.25:');
+    writeln('Current Version: 1.0.26:');
+    writeln('This version is a small update.');
+    writeln('Add new maths: (a-b)^3, (a+b)^3, a^3 + b^3');
+    writeln('');
+    readln();
+   //1.0.25
+    writeln('Available old version (not too old yet): 1.0.25')
     writeln('1.TextColor now available! When you use the program, TextColor will change the text color to the green,red, or white (default). Id like to make this app use GUI, but I dont have enough time to do this.');
     writeln('2.Change label exit -> exit_program to avoid unexpected error(!?)');
     writeln('3.Now you can install this application (oh, this is not important because you can share the binary file to other without any installer) via .exe setup file (yes, of course). I will create another installer for *NIX (Linux, BSD and macOS:)');
@@ -254,7 +311,7 @@ cal:
     writeln('5.Added Know issuses');
     writeln('6.Added some documents for you (not available now yet.)');
     readln();
-
+   //1.0.2
     WriteLn('Old Version: 1.0.2:');
     WriteLn('1.Add new features: some advanced math(s), add more choices for exit');
     WriteLn('2.Fix goto exit -> goto Exit bug on line 228 and bugs while building');
@@ -274,7 +331,7 @@ cal:
     writeln('Press Enter...');
     Readln();
   //Know issuse(s)
-    writeln('1. When users wants to exit the program, they will see the blinking cursor and nothing more happended. See the exit_program -> sub_choice=1 (exit) in source code for more details. User still can exit by type 2 and press Enter.');
+    writeln('1. When users wants to exit the program, they will see the blinking cursor and nothing more happended. See the video in Github (lebao3105/app1cli). User still can exit by type 2 and press Enter or use Alt + F4.
     writeln('2. *nothing more*');
     writeln('I will find and gather more bug and issuses here...');
     WriteLn('"All done. Press Enter to exit..."');
@@ -328,7 +385,7 @@ cal:
        if sub_choice = 3 then goto exit_program;
   end;
 //exit
-exit_program: //change from exit to avoid error
+exit_program:
    writeln('Are you sure want to exit the program? 1.Yes 2.No');
    write('Your choice: '); readln(sub_choice);
     if sub_choice = 2 then

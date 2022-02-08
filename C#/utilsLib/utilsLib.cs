398304c249525d338da696b1207cbe0037411cfc
Le Bao Nguyen <bao12345yocoo@gmail.com>
2022-02-08 22:47:37 +0700

    Update C# program, will recreate .sln file tomorrow (not fully fixed all errors yet)

--- C#/utilsLib/utilsLib.cs
+++ C#/utilsLib/utilsLib.cs
@@ -5,15 +5,40 @@ namespace utilsLib;
 
 public class utilsLibrary
 {
-    public void ask_2nums(int target1, int target2) {
+    public int target1;
+    public int target2;
+    public int target3;
+    public void ask_2nums(int target1, int target2, string? check) {
         dev.Write("Enter a value for the first target: ");
         target1 = Convert.ToInt32(dev.ReadLine());
         dev.Write("And the second target too: ");
         target2 = Convert.ToInt32(dev.ReadLine());
+        // add a fix for no-value numbers
+        if (check == "compare") 
+        {
+            compare(target1, target2);
+        } 
+        else if (check == "add") 
+        {
+            dev.WriteLine("The answer is: "+(target1+target2));
+        } 
+        else if (check == "minus") 
+        {
+            dev.WriteLine("The answer is: "+(target1+target2));
+        } 
+        else if (check == "multiple") 
+        {
+            dev.WriteLine("The answer is: "+(target1+target2));
+        } 
+        else if (check == "div") 
+        {
+            dev.WriteLine("The answer is: "+(target1+target2));
+        }
+
     }
 
     public void ask_3nums(int target1, int target2, int target3) {
-        ask_2nums(target1, target2);
+        ask_2nums(target1, target2, null);
         dev.Write("The third target: ");
         target3 = Convert.ToInt32(dev.ReadLine());
     }

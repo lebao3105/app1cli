ae932f4e6460915b9a9edd5be9d784a5d77f7cf0
lebao3105 <bao12345yocoo@gmail.com>
2022-04-07 21:52:20 +0700

    Update files

--- AUR_Pkgs/sellang.sh
+++ AUR_Pkgs/sellang.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
-if ! command -v whiptail &> /dev/null
-then
-    pacman -Syy whiptail
-    break
-else 
-    break
-fi
+## Script that ask the user for replacing app1 language
+## Current available langs: English and Vietnamese
 
-if (whiptail --title "Choose language" \
-        --mgsbox "This widzard will help you choose your language for app1. Press Enter to continue." \
-        10 60) then
-    break
-else
-    exit 1
-fi
+# Create some variable
+DIALOG_CANCEL=1
+DIALOG_ESC=255
+HEIGHT=0
+WIDTH=0
 
-Option=$(whiptail --title "Choose language" \
-            --menu "Choose your prefered language here" 15 60 4 \
-            "1" "Vietnamese" \
-            "2" "English" \
-            "3" "Both" 3>&1 1>&2 2>&3)
-exitstatus = $?
-if [ exitstatus = 0 ]; 
-then
-    echo $Option
-else
-    break
-fi
-    
+# Create a dialog
+dialog \
+    --title "Language Chooser" \
+    --clear --cancel-label "Exit" \
+    --menu "Please choose the language you want to use for app1:" \
+    $HEIGHT $WIDTH 4 \
+    "1" "English (Default)" \
+    "2" "Vietnamese" 2>&1 1>&3
+
+exit_status=$?
+case $exit_status in
+    $DIALOG_CANCEL)
+        clear
+        echo "Canceled. Now we will compile the code in English."
+        ;;
+    $DIALOG_ESC)
+        clear
+        echo "Canceled. Now we will compile the code in English."
+        ;;
+esac
+case $selection in
+    1)
+        clear
+        echo "Now we will compile the code in English."
+        ;;
+    2)
+        clear
+        echo "Now we will compile the code in Vietnamese."
+        sed -i 's/lang_en/lang_vi/g' "${srcdir}/*.pas"
+        ;;
+esac
 
 

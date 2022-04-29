e02035b69b0dde4f53736b60e4c63d09197cbc43
lebao3105 <bao12345yocoo@gmail.com>
2022-04-29 20:22:32 +0700

    A new library: mathsLib. Also added useful .gitignore files

---
+++ C#/mathsLib/mathsLib.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using dev = System.Console;
+namespace mathsLib;
+public class mathsLibrary
+{
+    private const string line1 = "The answer is: ";
+    private const string line2 = "Here's what you are looking for - the answer is: ";
+
+    public void basic_operations(int number1, int number2, string ? check) {
+        if (check == "add") {
+            dev.WriteLine(line1 + (number1 + number2));
+        } else if (check == "sub") {
+            dev.WriteLine(line2 + (number1 - number2));
+        } else if (check == "multiple") {
+            dev.WriteLine(line1 + (number1 * number2));
+        } else if (check == "divide") {
+            dev.WriteLine(line2 + (number1 / number2));
+        } 
+    }
+}
